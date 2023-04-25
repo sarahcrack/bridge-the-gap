5cbd6379215d86e58d6fdaaa186e3b7d065c74e3
UmamahBegg <umamah.begg@gmail.com>
2023-04-25 16:48:41 +0300

    Contribute/Display

--- bridge-the-gap/src/App.js
+++ bridge-the-gap/src/App.js
@@ -16,9 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar />
-      <ChooseCategory />
-      <ContributeResource />
+      <ContributeResource onContribute={handleContribution}/>
       <DisplayResources resources={resources} />
     </div>
   );
--- bridge-the-gap/src/components/DisplayResources/index.js
+++ bridge-the-gap/src/components/DisplayResources/index.js
@@ -1,4 +1,21 @@
-function DisplayResources() {
-  return <div>Display Resources</div>;
+function DisplayResources({resources}) {
+  return (
+  // Map through the resources that have been passed down from the APP
+  // Display the resources one by one - resources.contributor, resources.category, resources.title, resources.description, resources.link
+  <div>
+    {resources.map((resource, index) => (
+      <div className="resource-card">
+        <h3>{resource.contributor}</h3>
+        <h4>{resource.category}</h4>
+        <h4>{resource.title}</h4>
+        <p>{resource.description}</p>
+        <p><strong>Link:</strong> <a href={resource.link}>{resource.link}</a></p>
+
+        </div>
+    ))}
+  </div>
+  )
 }
+
 export default DisplayResources;
+
