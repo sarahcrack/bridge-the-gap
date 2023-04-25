7e4f02feb7bddd432814b68e6ac90d8b60e21f89
UmamahBegg <umamah.begg@gmail.com>
2023-04-25 17:46:29 +0300

    ImageButtons

--- bridge-the-gap/src/components/ChooseCategory/index.js
+++ bridge-the-gap/src/components/ChooseCategory/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import tutorials from '../src/Images/i-tutorials.png';
-
+import coding from '../../Images/i-coding.png';
+import design from '../../Images/i-design.jpeg';
+import collaboration from '../../Images/i-collaboration.png';
+import planning from '../../Images/i-planning.png';
+import tutorials from '../../Images/i-tutorials.png';
+import career from '../../Images/i-career.png';
 
 function ChooseCategory() {
   return (
     <div>
+      <button> <img src={coding} alt="Coding" /> </button>
+      <button> <img src={design} alt="Design" /> </button>
+      <button> <img src={collaboration} alt="Collaboration" /> </button>
+      <button> <img src={planning} alt="Planning" /> </button>
       <button> <img src={tutorials} alt="Tutorial" /> </button>
-      <button>Design</button>
-      <button>Tutorials</button>
-      <button>Collaborative Tools</button>
+      <button> <img src={career} alt="Careers" /> </button>
+  
     </div>
   );
 }
