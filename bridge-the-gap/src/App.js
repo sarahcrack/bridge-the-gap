367d55a76ca7b90378727c113083799414e4b418
abimbolaehighebolo <bim.ehighebolo@gmail.com>
2023-04-26 10:22:13 +0100

    added some resource contributions

--- bridge-the-gap/src/App.js
+++ bridge-the-gap/src/App.js
@@ -10,7 +10,27 @@ import DisplayResources from "./components/DisplayResources";
 
 function App() {
   // create a resources state to pass down to DisplayResources
-  const [resources, setResources] = useState([]);
+  const [resources, setResources] = useState([{contributor:'bim',
+  category: 'Design',
+  title: 'css tricks',
+  description: 'handy cheatsheet for css',
+  link: 'css-tricks.com'
+   }, {
+    contributor:'paul',
+    category: 'Collaboration',
+    title: 'trello',
+    description: 'create for writing to do lists as tickets for project managing',
+    link: 'trello.com'
+  }, {
+    contributor:'sarah',
+    category: 'Tutorial',
+    title: 'react tutorial for beginners',
+    description: 'react JS tutorials by Programming with Mosh',
+    link: 'https://youtu.be/SqcYOGIETPk'
+  }
+  ]);
+
+
   // create a function to update the resources state called handleContribution
   function handleContribution(contribution) {
     setResources([...resources, contribution]);
