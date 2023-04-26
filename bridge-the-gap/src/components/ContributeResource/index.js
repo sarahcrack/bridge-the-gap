983b3b03fe91008605f16ae61b7d37079388125c
sarahcrack <sarah.l.crack@googlemail.com>
2023-04-26 10:47:56 +0100

    app now working across multiple pages - branches merged

--- bridge-the-gap/src/components/ContributeResource/index.js
+++ bridge-the-gap/src/components/ContributeResource/index.js
@@ -2,16 +2,11 @@ import { useState, useEffect } from "react";
 
 function ContributeResource({ resources, setResources }) {
   const [contributor, setContributor] = useState("");
-
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [link, setLink] = useState("");
   const [category, setCategory] = useState("");
   const [categoryList, setCategoryList] = useState([
-
-
-  //make an array with categories and map through it to create a dropdown menu
-  const [categories, setCategories] = useState([
     "Coding",
     "Design",
     "Collaboration",
@@ -19,7 +14,6 @@ function ContributeResource({ resources, setResources }) {
     "Tutorials",
     "Careers",
   ]);
-
 
   function handleContribute(resource) {
     setResources([...resources, resource]);
@@ -30,22 +24,11 @@ function ContributeResource({ resources, setResources }) {
     handleContribute({
       contributor,
       category,
-
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [link, setLink] = useState("");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    onContribute({
-      contributor,
-      category,
       title,
       description,
       link,
     });
-
+    //console.log(contributor, category, title, description, link);
     setContributor("");
     setCategory("");
     setTitle("");
@@ -103,3 +86,106 @@ function ContributeResource({ resources, setResources }) {
 }
 
 export default ContributeResource;
+
+// import { useState, useEffect } from "react";
+
+// function ContributeResource({ resources, setResources }) {
+//   const [contributor, setContributor] = useState("");
+
+//   const [title, setTitle] = useState("");
+//   const [description, setDescription] = useState("");
+//   const [link, setLink] = useState("");
+//   const [category, setCategory] = useState("");
+
+//   //make an array with categories and map through it to create a dropdown menu
+//   const [categoryList, setCategoryList] = useState([
+//     "Coding",
+//     "Design",
+//     "Collaboration",
+//     "Planning",
+//     "Tutorials",
+//     "Careers",
+//   ]);
+
+//   function handleContribute(resource) {
+//     setResources([...resources, resource]);
+//   }
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     handleContribute({
+//       contributor,
+//       category,
+
+//   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+//   const [title, setTitle] = useState("");
+//   const [description, setDescription] = useState("");
+//   const [link, setLink] = useState("");
+
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     onContribute({
+//       contributor,
+//       category,
+//       title,
+//       description,
+//       link,
+//     });
+
+//     setContributor("");
+//     setCategory("");
+//     setTitle("");
+//     setDescription("");
+//     setLink("");
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <h2 className="contribute-header">Contribute a Resource</h2>
+//       <label htmlFor="contributor">Contributor</label>
+//       <input
+//         type="text"
+//         id="contributor"
+//         value={contributor}
+//         onChange={(event) => setContributor(event.target.value)}
+//       />
+//       <label htmlFor="category">Category</label>
+//       <select
+//         id="category"
+//         value={category}
+//         onChange={(event) => setCategory(event.target.value)}
+//       >
+//         {categoryList.map((cat) => (
+//           <option value={cat} key={Math.random()}>
+//             {cat}
+//           </option>
+//         ))}
+//       </select>
+
+//       <label htmlFor="title">Title</label>
+//       <input
+//         type="text"
+//         id="title"
+//         value={title}
+//         onChange={(event) => setTitle(event.target.value)}
+//       />
+//       <label htmlFor="description">Description</label>
+//       <input
+//         type="text"
+//         id="description"
+//         value={description}
+//         onChange={(event) => setDescription(event.target.value)}
+//       />
+//       <label htmlFor="link">Link</label>
+//       <input
+//         type="text"
+//         id="link"
+//         value={link}
+//         onChange={(event) => setLink(event.target.value)}
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export default ContributeResource;
