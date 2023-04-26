2713b9fafa0a7527c35e07b7f0ada956cf27a34d
UmamahBegg <umamah.begg@gmail.com>
2023-04-26 18:48:22 +0300

    thankYou

--- bridge-the-gap/src/components/ThankYou/index.js
+++ bridge-the-gap/src/components/ThankYou/index.js
@@ -11,12 +11,17 @@ function ThankYou() {
     <div className="thank-you-container">
       <h2>Thank you for your contribution!</h2>
       <p>You’ve helped to bridge the gap for your fellow bootcampers!</p>
-      <p>Dont forget to share your contribution with other bootcampers! 🤩</p>
+
       <img src={blackArrow} alt="black arrow" className="arrow-image" />
-      <div className="social-media-icons">
-        <img src={facebook} alt="facebook logo" className="facebook-image" />
-        <img src={instagram} alt="instagram logo" className="instagram-image" />
-        <img src={linkedIn} alt="linkedin logo" className="linkedin-logo" />
+      <div className="social-media-container">
+        <div className="social-media-icons-container">
+        <p> Dont forget to share your contribution with other bootcampers! 🤩</p>
+          <div className="social-media-icons">
+            <img src={facebook} alt="facebook logo" className="facebook-image" />
+            <img src={instagram} alt="instagram logo" className="instagram-image" />
+            <img src={linkedIn} alt="linkedin logo" className="linkedin-logo" />
+          </div>
+        </div>
       </div>
       <Link to="/display">
         <button className="click-for-contribution-button">
