24c3fafa9b322dbae1e799dbd4a60b2a8a766738
percygr <percyg@gmail.com>
2023-04-26 15:37:35 +0100

    removed old non-working code

--- bridge-the-gap/src/components/ContributeResource/index.js
+++ bridge-the-gap/src/components/ContributeResource/index.js
@@ -21,17 +21,6 @@ function ContributeResource({ resources, setResources }) {
   const navigate = useNavigate();
 
   function handleContribute(resource) {
-    // //add http:// to link if it doesn't exist
-    // let link = resource.link;
-    // if (!link.includes("http://") && !link.includes("https://")) {
-    //   link = "https://" + link;
-    // }
-
-    // //update resources object with new link value
-    // //resource.link = link;
-    // setLink(link);
-    // console.log(resource.link);
-
     setResources([...resources, resource]);
   }
 
