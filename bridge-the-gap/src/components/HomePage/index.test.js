10d6902c509c93f13fec436558d85d46d967edf8
percygr <percyg@gmail.com>
2023-04-27 09:38:47 +0100

    page title & icon

--- bridge-the-gap/src/components/HomePage/index.test.js
+++ bridge-the-gap/src/components/HomePage/index.test.js
@@ -2,11 +2,16 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { test, expect } from "@jest/globals";
 import userEvent from "@testing-library/user-event";
-import ChooseCategory from "./";
+import HomePage from "./";
 import { BrowserRouter } from "react-router-dom";
 
 // check the page renders an image with alt text 'Coding'
 test("renders Coding image", async () => {
-  render(<ChooseCategory />, { wrapper: BrowserRouter });
+  render(<HomePage />, { wrapper: BrowserRouter });
   const user = userEvent.setup();
+
+  // verify page content for default route
+  expect(
+    screen.getByText(/Bridge the gap in your knowledge!/i)
+  ).toBeInTheDocument();
 });
